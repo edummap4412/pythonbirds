bd3ae7dbf5f011ce1e19ef08e6dc01bb68b6c72e
Eduardo Programador <eduardomichael1@gmail.com>
2021-03-27 21:35:39 -0300

    Criados atributos de instancia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,18 @@
 class Pessoa:
+    def __init__(self, nome=None,idade=35):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Eduardo')
     print(p.cumprimentar())
-    print(id(p))
+    print(p.nome)
+    p.nome ='Renzo'
+    print(p.nome)
+    print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
+
